83b97ed96007dbe572c88a74f6534994307ea28b
Andrey Melnik <druid.bpg@gmail.com>
2012-07-19 19:03:28 +0300

    Initial Commit

---
+++ Banana Booth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Banana Booth
+//
+//  Created by Andrey Melnik on 19.07.12.
+//  Copyright (c) 2012 Unteleported. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
