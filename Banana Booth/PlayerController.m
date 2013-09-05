af19eea270303c8e68a80a4a9301659d2915c26d
Andrey Melnik <andrey.melnik@icloud.com>
2013-09-05 18:44:25 +0300

    New movie

--- Banana Booth/PlayerController.m
+++ Banana Booth/PlayerController.m
@@ -6,7 +6,9 @@
 //  Copyright (c) 2012 Unteleported. All rights reserved.
 //
 
-#import "PlayerController.h"
+#import "PlayerController.h"   
+#import <QuartzCore/CAAnimation.h>
+#import <QuartzCore/CAMediaTimingFunction.h>
 
 @interface PlayerController ()
 
@@ -34,24 +36,32 @@
     [super viewDidLoad];
     
     NSBundle *bundle = [NSBundle mainBundle];
-    NSString *moviePath = [bundle pathForResource:@"Animation_Naked_Banana_v1_1" ofType:@"mov"];
+    NSString *moviePath = [bundle pathForResource:@"Naked_banana_dance1" ofType:@"mov"];
     NSURL *movieURL = [NSURL fileURLWithPath:moviePath];
     
     face.image = face_image;
     face.frame = face_rect;
     
     CGRect rect = face.frame;
-    rect.origin.x = 160;
-    rect.origin.y = 200;
+    rect.origin.x = 0;
+    rect.origin.y = 0;
     rect.size.width = rect.size.width/2;
     rect.size.height =  rect.size.height/2;
     
     face.frame = rect;
     
+//    UIGraphicsBeginImageContext(self.bounds.size);
+//    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+//    UIImage *viewImage = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+    
     player = [[MPMoviePlayerController alloc] initWithContentURL: movieURL];
     [player prepareToPlay];
     [player.view setFrame: self.view.bounds];
+    player.allowsAirPlay = NO;
     [self.view insertSubview: player.view belowSubview: back_button];
+    
+    [face.layer addAnimation:[self animate] forKey:nil];
     
     player.view.userInteractionEnabled = NO;
 
@@ -60,6 +70,22 @@
 //    [player setFullscreen:YES animated:YES];
 //    [player initWithContentURL];
 //    [player play];
+}
+
+- (CAAnimation*) animate {
+	CAKeyframeAnimation* animation = [CAKeyframeAnimation animationWithKeyPath:@"transform"];
+    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear];
+    animation.values = [NSArray arrayWithObjects:
+        [NSValue valueWithCATransform3D:CATransform3DRotate(CATransform3DMakeTranslation(44.60, 93.65, 0), 0, 1, 1, 1)],
+        [NSValue valueWithCATransform3D:CATransform3DRotate(CATransform3DMakeTranslation(44.60, 86.00, 0), 0, 1, 1, 1)],
+        [NSValue valueWithCATransform3D:CATransform3DRotate(CATransform3DMakeTranslation(27.65, 96.30, 0), -7.0, 1, 1, 1)],
+        [NSValue valueWithCATransform3D:CATransform3DRotate(CATransform3DMakeTranslation(200, 257, 0), 0, 1, 1, 1)],
+        [NSValue valueWithCATransform3D:CATransform3DRotate(CATransform3DMakeTranslation(-8, 17, 0), 0, 1, 1, 1)],
+        [NSValue valueWithCATransform3D:CATransform3DRotate(CATransform3DMakeTranslation(-8, 17, 0), 0, 1, 1, 1)],
+    nil];	
+    
+    animation.duration = 10.2;
+    return animation;
 }
 
 - (void)didReceiveMemoryWarning
